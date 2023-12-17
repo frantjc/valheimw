81e71980b20a6627bfab015955aa561e845c3cd2
Alex Logsdon <39865011+frantjc@users.noreply.github.com>
2023-12-17 12:03:29 -0500

    more flags (#12)
    * more flags
    
    * more valheim args

--- valheim/opts.go
+++ valheim/opts.go
@@ -2,17 +2,127 @@ package valheim
 
 import (
 	"fmt"
+	"path/filepath"
+	"time"
+)
+
+type Preset string
+
+func (p Preset) String() string {
+	return string(p)
+}
+
+var (
+	PresetNormal    Preset = "normal"
+	PresetCasual    Preset = "casual"
+	PresetEasy      Preset = "easy"
+	PresetHard      Preset = "hard"
+	PresetHardcore  Preset = "hardcore"
+	PresetImmersive Preset = "immersive"
+	PresetHammer    Preset = "hammer"
+)
+
+type CombatModifier string
+
+func (m CombatModifier) String() string {
+	return string(m)
+}
+
+var (
+	CombatModifierVeryEasy CombatModifier = "veryeasy"
+	CombatModifierEasy     CombatModifier = "easy"
+	CombatModifierHard     CombatModifier = "hard"
+	CombatModifierVeryHard CombatModifier = "veryhard"
+)
+
+type DeathPenaltyModifier string
+
+func (m DeathPenaltyModifier) String() string {
+	return string(m)
+}
+
+var (
+	DeathPenaltyModifierCasual   DeathPenaltyModifier = "casual"
+	DeathPenaltyModifierVeryEasy DeathPenaltyModifier = "veryeasy"
+	DeathPenaltyModifierEasy     DeathPenaltyModifier = "easy"
+	DeathPenaltyModifierHard     DeathPenaltyModifier = "hard"
+	DeathPenaltyModifierHardcore DeathPenaltyModifier = "hardcore"
+)
+
+type ResourceModifier string
+
+func (m ResourceModifier) String() string {
+	return string(m)
+}
+
+var (
+	ResourceModifierMuchLess ResourceModifier = "muchless"
+	ResourceModifierLess     ResourceModifier = "less"
+	ResourceModifierMore     ResourceModifier = "more"
+	ResourceModifierMuchMore ResourceModifier = "muchmore"
+	ResourceModifierMost     ResourceModifier = "most"
+)
+
+type RaidModifier string
+
+func (m RaidModifier) String() string {
+	return string(m)
+}
+
+var (
+	RaidModifierNone     RaidModifier = "none"
+	RaidModifierMuchLess RaidModifier = "muchless"
+	RaidModifierLess     RaidModifier = "less"
+	RaidModifierMore     RaidModifier = "more"
+	RaidModifierMuchMore RaidModifier = "muchmore"
+)
+
+type PortalModifier string
+
+func (m PortalModifier) String() string {
+	return string(m)
+}
+
+var (
+	PortalModifierCasual   PortalModifier = "casual"
+	PortalModifierHard     PortalModifier = "hard"
+	PortalModifierVeryHard PortalModifier = "veryhard"
 )
 
 // Opts is a helper struct to build arguments
 // to pass to the Valheim executable.
 type Opts struct {
+	Name     string
 	Port     int64
 	World    string
-	Name     string
-	SaveDir  string
 	Password string
-	Public   bool
+	SaveDir  string
+
+	Public bool
+
+	LogFile string
+
+	SaveInterval time.Duration
+	Backups      int64
+	BackupShort  time.Duration
+	BackupLong   time.Duration
+
+	Crossplay bool
+
+	InstanceID string
+
+	Preset
+
+	CombatModifier
+	DeathPenaltyModifier
+	ResourceModifier
+	RaidModifier
+	PortalModifier
+
+	NoBuildCost  bool
+	PlayerEvents bool
+	PassiveMobs  bool
+	NoMap        bool
 }
 
 // ToArgs transforms Opts into an array
@@ -20,16 +130,12 @@ type Opts struct {
 func (o *Opts) ToArgs() []string {
 	args := []string{}
 
-	if o.SaveDir != "" {
-		args = append(args, "-savedir", o.SaveDir)
+	if o.Name != "" {
+		args = append(args, "-name", o.Name)
 	}
 
 	if o.Port != 0 {
 		args = append(args, "-port", fmt.Sprint(o.Port))
-	}
-
-	if o.Name != "" {
-		args = append(args, "-name", o.Name)
 	}
 
 	if o.World != "" {
@@ -40,8 +146,80 @@ func (o *Opts) ToArgs() []string {
 		args = append(args, "-password", o.Password)
 	}
 
-	if o.Public {
-		args = append(args, "-public", "1")
+	if o.SaveDir != "" {
+		args = append(args, "-savedir", o.SaveDir)
+	}
+
+	if !o.Public {
+		args = append(args, "-public", "0")
+	}
+
+	if o.LogFile != "" {
+		args = append(args, "-logFile", filepath.Clean(o.LogFile))
+	}
+
+	if o.SaveInterval != 0 {
+		args = append(args, "-saveinterval", fmt.Sprint(int64(o.SaveInterval.Seconds())))
+	}
+
+	if o.Backups != 0 {
+		args = append(args, "-backups", fmt.Sprint(o.Backups))
+	}
+
+	if o.BackupShort != 0 {
+		args = append(args, "-backupshort", fmt.Sprint(int64(o.BackupShort.Seconds())))
+	}
+
+	if o.SaveInterval != 0 {
+		args = append(args, "-backuplong", fmt.Sprint(int64(o.BackupLong.Seconds())))
+	}
+
+	if o.Crossplay {
+		args = append(args, "-crossplay")
+	}
+
+	if o.InstanceID != "" {
+		args = append(args, "-instanceid", o.InstanceID)
+	}
+
+	if o.Preset != "" {
+		args = append(args, "-preset", string(o.Preset))
+	}
+
+	if o.CombatModifier != "" {
+		args = append(args, "-modifier", "combat", string(o.CombatModifier))
+	}
+
+	if o.DeathPenaltyModifier != "" {
+		args = append(args, "-modifier", "deathpenalty", string(o.DeathPenaltyModifier))
+	}
+
+	if o.ResourceModifier != "" {
+		args = append(args, "-modifier", "resources", string(o.ResourceModifier))
+	}
+
+	if o.RaidModifier != "" {
+		args = append(args, "-modifier", "raids", string(o.RaidModifier))
+	}
+
+	if o.PortalModifier != "" {
+		args = append(args, "-modifier", "portals", string(o.PortalModifier))
+	}
+
+	if o.NoBuildCost {
+		args = append(args, "-setkey", "nobuildcost")
+	}
+
+	if o.PlayerEvents {
+		args = append(args, "-setkey", "playerevents")
+	}
+
+	if o.PassiveMobs {
+		args = append(args, "-setkey", "passivemobs")
+	}
+
+	if o.NoMap {
+		args = append(args, "-setkey", "nomap")
 	}
 
 	return args
